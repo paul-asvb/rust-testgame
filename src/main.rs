24565e8d75e44e56ffc4065ec743c19ec4d27a70
Paul Mayer <paul@paul-mayer.eu>
2021-09-15 22:56:12 +0200

    1-2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+use cgmath::Vector2;
 use macroquad::prelude::*;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let mut x: f32 = 100.0;
-    let mut y: f32 = 100.0;
-    let mut xspeed: f32 = 10.0;
-    let mut yspeed: f32 = 10.0;
+    let mut position = Vector2 { x: 20.0, y: 20.0 };
+    let mut speed = Vector2 { x: 10.0, y: 10.0 };
     const SIZE: f32 = 20.0;
 
     loop {
@@ -15,17 +14,16 @@ async fn main() {
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
-        x = x + xspeed;
-        y = y + yspeed;
+        position = position + speed;
 
-        if (x > screen_width() - SIZE) || (x < SIZE) {
-            xspeed = xspeed * -1.0;
+        if (position.x > screen_width() - SIZE) || (position.x < SIZE) {
+            speed.x = speed.x * -1.0;
         }
-        if (y > screen_height() - SIZE) || (y < SIZE) {
-            yspeed = yspeed * -1.0;
+        if (position.y > screen_height() - SIZE) || (position.y < SIZE) {
+            speed.y = speed.y * -1.0;
         }
 
-        draw_circle(x, y, SIZE, YELLOW);
+        draw_circle(position.x, position.y, SIZE, YELLOW);
 
         draw_text("press enter!", 20.0, 20.0, 30.0, DARKGRAY);
         if is_key_down(KeyCode::Right) {
