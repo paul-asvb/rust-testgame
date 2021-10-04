7893aeb377fc8f6e824229f08afcfc2dd0f6090f
Paul <paul@paul-mayer.eu>
2021-10-04 16:16:15 +0200

    add rotation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use cgmath::{Deg, Matrix2, Point2, Rad, Rotation, Rotation2};
+use cgmath::{
+    num_traits::float, Basis2, Deg, Matrix2, Point2, Rad, Rotation, Rotation2, Vector1, Vector2,
+};
 use macroquad::prelude::*;
 use snake::Snake;
 mod snake;
@@ -6,23 +8,24 @@ mod snake;
 #[macroquad::main("BasicShapes")]
 async fn main() {
     let zoom = 1.0;
-    let location = vec2(0.0, 0.0);
-    let mut dir = vec2(0.1, 0.0);
+    let location = Vector2::new(0.0, 0.0);
+    let mut dir = Vector2::new(0.1, 0.0);
     loop {
         set_camera(&Camera2D {
             zoom: vec2(zoom, screen_width() / screen_height()),
             ..Default::default()
         });
 
-        //let deg: Deg<f32> = Deg(180.0);
+        let deg: Deg<f32> = Deg(547.0);
 
-        //let bla: Matrix2<Point2<f32>> = Rotation2::from_angle(Rad(0.5f64 * std::f64::consts::PI));
+        let rot = Basis2::from_angle(deg);
+        // let rotation: Basis2<f64> = Rotation2::from_angle(Rad(0.5f64 * std::f64::consts::PI));
 
-        //bla.rotate_vetor(dir);
+        dir = rot.rotate_vector(dir);
 
-        if is_key_down(KeyCode::Right) {
-            dir = dir + vec2(0.1, 0.1);
-        }
+        // if is_key_down(KeyCode::Right) {
+        //     dir = dir + vec2(0.1, 0.1);
+        // }
 
         draw_line(location.x, location.y, dir.x, dir.y, 0.01, BLUE);
 
