8c40ec27fe9333213fb8cde0b4e926a3946fff5d
paul-asvb <paul-asvb@users.noreply.github.com>
2021-10-12 08:13:43 +0000

    deploy: 4ebc418b4fea6c0fcac45d6c882ceaf0ba344d10

--- deps/testgame.d
+++ deps/testgame.d
@@ -1,6 +1,7 @@
-/home/runner/work/rust-testgame/rust-testgame/target/wasm32-unknown-unknown/release/deps/testgame.wasm: src/main.rs src/snake.rs
+/home/runner/work/rust-testgame/rust-testgame/target/wasm32-unknown-unknown/release/deps/testgame.wasm: src/main.rs src/game.rs src/snake.rs
 
-/home/runner/work/rust-testgame/rust-testgame/target/wasm32-unknown-unknown/release/deps/testgame.d: src/main.rs src/snake.rs
+/home/runner/work/rust-testgame/rust-testgame/target/wasm32-unknown-unknown/release/deps/testgame.d: src/main.rs src/game.rs src/snake.rs
 
 src/main.rs:
+src/game.rs:
 src/snake.rs:
--- testgame.d
+++ testgame.d
@@ -1,1 +1,1 @@
-/home/runner/work/rust-testgame/rust-testgame/target/wasm32-unknown-unknown/release/testgame.wasm: /home/runner/work/rust-testgame/rust-testgame/src/main.rs /home/runner/work/rust-testgame/rust-testgame/src/snake.rs
+/home/runner/work/rust-testgame/rust-testgame/target/wasm32-unknown-unknown/release/testgame.wasm: /home/runner/work/rust-testgame/rust-testgame/src/game.rs /home/runner/work/rust-testgame/rust-testgame/src/main.rs /home/runner/work/rust-testgame/rust-testgame/src/snake.rs
