b2e828e77327095352ce9015af0989ddd45a4731
Paul <paul@paul-mayer.eu>
2021-10-12 09:23:57 +0200

    add game

---
+++ src/game.rs
@@ -0,0 +1,32 @@
+use macroquad::prelude::{is_key_down, KeyCode};
+
+use crate::snake;
+pub struct Game {
+    snakes: Vec<snake::Snake>,
+}
+
+pub fn init_game(number_of_snakes: i8) -> Game {
+    Game {
+        snakes: (0..number_of_snakes)
+            .map(|_| snake::new_snake())
+            .collect(),
+    }
+}
+
+impl Game {
+    pub fn update(mut self) {
+        for snake in self.snakes.into_iter() {
+            if is_key_down(KeyCode::Right) {
+               // snake.right();
+            }
+
+            if is_key_down(KeyCode::Left) {
+               // snake.left();
+            }
+
+           // snake.add();
+
+            //snake.draw();
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
+use game::Game;
 use macroquad::prelude::*;
-use snake::new_snake;
+mod game;
 mod snake;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
     let zoom = 1.0;
-    let mut snake = new_snake();
+    let game = game::init_game(2);
+    game.update();
 
     loop {
         set_camera(&Camera2D {
-            zoom: vec2(zoom, screen_width() / screen_height()),
+            zoom: vec2(zoom, screen_width() / screen_height() * zoom),
             ..Default::default()
         });
 
-        if is_key_down(KeyCode::Right) {
-            snake.right();
-        }
-
-        if is_key_down(KeyCode::Left) {
-            snake.left();
-        }
-
-        snake.add();
-
-        snake.draw();
 
         set_default_camera();
         next_frame().await
