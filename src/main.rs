0b58e305cda7b76e7d470c7af393bb8f8b6ade53
Paul Mayer <paul@paul-mayer.eu>
2022-03-01 21:42:17 +0100

    cargo upgrade

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,22 @@ use macroquad::prelude::*;
 mod game;
 mod snake;
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("zatake")]
 async fn main() {
     let zoom = 1.0;
-    let mut game = game::init_game(10);
+    let mut game = game::init_game(100);
 
     loop {
         //clear_background(RED);
-        /*set_camera(&Camera2D {
+        set_camera(&Camera2D {
             zoom: vec2(zoom, zoom),
             ..Default::default()
-        });*/
-        //set_default_camera();
+        });
+       // set_default_camera();
 
         game.render();
 
-        //set_default_camera();
+        set_default_camera();
         next_frame().await
     }
 }
