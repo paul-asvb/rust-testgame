187f729c95828b93eb71cd80e300b5c61e8d4f86
Paul Mayer <paul@paul-mayer.eu>
2022-03-06 19:09:29 +0100

    clearing tests

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ mod snake;
 
 #[macroquad::main("zatake")]
 async fn main() {
-    let zoom = 1.0;
-    let mut game = game::init_game(100);
+    let zoom = 1000.0;
+    //let mut game = game::init_game(100);
 
     loop {
         //clear_background(RED);
@@ -14,9 +14,16 @@ async fn main() {
             ..Default::default()
         });
 
-        game.render();
+        clear_background(RED);
 
-        set_default_camera();
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+
+        //game.render();
+
+        //set_default_camera();
         next_frame().await
     }
 }
