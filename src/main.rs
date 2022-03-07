5d6dc7508734deb3c39fa3356b0873dd676567ec
Paul Mayer <paul@paul-mayer.eu>
2022-03-07 20:55:34 +0100

    clear background bug

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,22 @@ mod snake;
 async fn main() {
     let zoom = 1000.0;
     //let mut game = game::init_game(100);
+    let mut y = 100.0;
 
     loop {
         //clear_background(RED);
-        set_camera(&Camera2D {
-            zoom: vec2(zoom, zoom),
-            ..Default::default()
-        });
+        // set_camera(&Camera2D {
+        //     zoom: vec2(zoom, zoom),
+        //     ..Default::default()
+        // });
 
-        clear_background(RED);
+        //clear_background(RED);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_line(50.0, y, 250.0, y, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
         draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
-
+        y = y + 1.0;
         //game.render();
 
         //set_default_camera();
